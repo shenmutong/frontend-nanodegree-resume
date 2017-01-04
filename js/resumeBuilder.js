5066128803f2a5defa0035922ebaa9cd7494f48f
shenmutong <shenmutong@gmail.com>
2017-01-04 17:25:07 +0800

    string.replace()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+$("#main").append("Shen Mutong")
+var awesomeThoughts = "I am shenmutong and I am AWESOME!"
+console.log(awesomeThoughts)
